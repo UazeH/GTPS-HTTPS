c0f8580cea70a0541c330f75367a29a4576bbd0a
UazeH <89394300+UazeH@users.noreply.github.com>
2022-06-29 10:53:41 +0700

    Update main.js

--- main.js
+++ main.js
@@ -15,6 +15,6 @@ const growtopia = https.createServer(options, function(req, res) {
       res.write(`server|127.0.0.1\nport|17091\ntype|1\n#maint|gtps\n\nbeta_server|127.0.0.1\nbeta_port|17091\n\nbeta_type|1\nmeta|ni.com\nRTENDMARKERBS1001`);
       console.log(`[GROWTOPIA] : Client ${ipAddress} send request to path ${req.url} with method ${req.method}`);
       res.end();
-}})
+}});
 
 growtopia.listen(443)
